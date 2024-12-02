d5d7b46ed89c7820fd5ce28479ec1ac46c064b54
Abish Issagulov <issagulov@kaist.ac.kr>
2024-12-03 00:21:16 +0900

    all cors allowed

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
-from services.firestore import test_firestore_connection
-from auth import create_access_token, verify_access_token
 from routers import user, plants
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url="/")
 
-@app.get("/")
-def root():
-    if test_firestore_connection():
-        return {"message": "Firestore is connected!"}
-    else:
-        return {"message": "Firestore connection failed!"}
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,  # Allow cookies to be included in requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user.router)
 app.include_router(plants.router)
